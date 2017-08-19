f4d81bdf80c2fd1bcb8f3723ea1a1b9f723222d1
alphadose <anish.mukherjee1996@gmail.com>
2017-08-20 01:12:39 +0530

    Added Asteroids

--- js/stateMain.js
+++ js/stateMain.js
@@ -1,6 +1,7 @@
 var block_vel= -160;
 var score=0;
 var scoreText;
+var asteroid_vel=-20;
 function End(block,hero){
 
   alert("Your score is "+score);
@@ -52,9 +53,10 @@ var StateMain = {
         this.hero.animations.play('right');
 
         cursors = game.input.keyboard.createCursorKeys();
-        game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR ]);
         this.blocks = game.add.group();
+        this.asteroids = game.add.group();
         this.makeBlocks();
+        this.makeAsteroids();
     },
 
     doJump: function() {
@@ -89,10 +91,40 @@ var StateMain = {
         });
         block_vel-=30;
     },
+    makeAsteroids: function() {
+        this.asteroids.removeAll();
+        var wallHeight = game.rnd.integerInRange(1, 6);
+        for (var i = 0; i < wallHeight; i++) {
+            var asteroid = game.add.sprite(150*(i), 0, "block");
+            asteroid.scale.setTo(1,1);
+
+            this.asteroids.add(asteroid);
+        }
+        this.asteroids.x = game.width - this.asteroids.width-120;
+        this.asteroids.y = this.ground.y - 400;
+
+
+        //Loop through each block
+        //and apply physics
+        this.asteroids.forEach(function(asteroid) {
+            //enable physics
+            game.physics.enable(asteroid, Phaser.Physics.ARCADE);
+            //set the x velocity to -160
+            asteroid.body.velocity.x = asteroid_vel;
+            asteroid.body.acceleration.x = -30*game.rnd.integerInRange(1,3);
+
+
+            asteroid.body.gravity.y =300;
+
+            asteroid.body.bounce.set(1, 1);
+        });
+        asteroid_vel-=30;
+    },
 
 
     update: function() {
         game.physics.arcade.collide(this.hero, this.ground);
+        game.physics.arcade.collide(this.hero, this.asteroids);
         //
         //collide the hero with the blocks
         //
@@ -101,11 +133,13 @@ var StateMain = {
         //colide the blocks with the ground
         //
         game.physics.arcade.collide(this.ground, this.blocks);
+        game.physics.arcade.collide(this.ground, this.asteroids);
         //
         //when only specifying one group, all children in that
         //group will collide with each other
         //
         game.physics.arcade.collide(this.blocks);
+        game.physics.arcade.collide(this.asteroids);
 
         if(cursors.right.isDown)
         {
@@ -127,13 +161,19 @@ var StateMain = {
           this.doJump();
 
         }
-
+        game.physics.arcade.overlap(this.blocks, this.hero,End, null, this);
         //get the first child
         var fchild = this.blocks.getChildAt(0);
-        game.physics.arcade.overlap(this.blocks, this.hero,End, null, this);
+
         //if off the screen reset the blocks
         if (fchild.x < -game.width) {
             this.makeBlocks();
+        }
+        var schild = this.asteroids.getChildAt(0);
+
+        //if off the screen reset the blocks
+        if (schild.x < -game.width) {
+            this.makeAsteroids();
         }
 
 
